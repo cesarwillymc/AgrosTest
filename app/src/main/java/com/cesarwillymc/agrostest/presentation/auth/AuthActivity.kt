1cf59d8574581861bee41ba522cd6db2b2f91713
Cesar Willy Mamani Canaza <cesarwilly.mc@gmail.com>
2021-08-03 09:34:36 -0500

    Adding dependencys and create activity

--- app/src/main/java/com/cesarwillymc/agrostest/presentation/auth/AuthActivity.kt
+++ app/src/main/java/com/cesarwillymc/agrostest/presentation/auth/AuthActivity.kt
@@ -1,11 +1,12 @@
-package com.cesarwillymc.agrostest
+package com.cesarwillymc.agrostest.presentation.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cesarwillymc.agrostest.R
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
     }
 }
